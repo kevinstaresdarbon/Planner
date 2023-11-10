0791c28c48df11104bd51e99e1bf3399e141bef4
kevinstaresdarbon <red.inshou@gmail.com>
2023-11-10 19:36:24 +0000

    adds bootstrap to the index.html and utilizes this in the generation of the calendar day in the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,19 @@ $(function(){
     for (let i=9; i < 18; i++){
        
         var hourEl = $('<div>');
-        hourEl.append($('<h2>' + i + '</h2>'));
         hourEl.attr('data-hour', i);
+        hourEl.addClass('d-flex flex-row justify-content-between row');
 
-        var inputEl = ('<input type="text"></input>');
+        var titleEl = $('<h2>' + i + '</h2>');
+        titleEl.addClass('col-2 col-md-1 d-flex justify-content-center align-items-center');
+        hourEl.append(titleEl);
+
+        var inputEl = $('<input type="text"></input>');
+        inputEl.addClass('col-8 col-md-10');
         hourEl.append(inputEl);
 
         var saveBtn = $('<button>Save</button>');
+        saveBtn.addClass('col-2 col-md-1');
         hourEl.append(saveBtn);
 
         containerEl.append(hourEl);
